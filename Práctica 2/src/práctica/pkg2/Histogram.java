297e09128c8aa9cbbde1604cc570d550ccef502c
Usuario <Usuario@10.22.148.82>
2019-10-02 09:10:47 +0100

    Kata1 using Date

--- Práctica 2/src/práctica/pkg2/Histogram.java
+++ Práctica 2/src/práctica/pkg2/Histogram.java
@@ -1,9 +1,20 @@
 package práctica.pkg2;
 
 public class Histogram {
+    private Object counts;
+    private final Map<Integer, Integer>
 
     Histogram(int... values) {
+        for (int value:values)
+            counts.put(value, counts.get(value)+1);
     }
     
+    public Set<Integer> getValues(){
+        return counts.ketSet();
+    }
 
+    public int getCount (int value){
+        return counts.get(value);
+    }
+    
 }

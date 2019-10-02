0a92adb4b12fb97addad8adf188f27b60f00a4fb
Usuario <Usuario@10.22.148.82>
2019-10-02 09:23:51 +0100

    Kata 2 arrays asociativas

--- Práctica 2/src/práctica/pkg2/Main.java
+++ Práctica 2/src/práctica/pkg2/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Histogram histogram = new Histogram (4,3,8,4,5,1);
         for (int value: histogram.getValues()){
-            System.out.println(value + " " + histogram.getCount())
+            System.out.println(value + " " + histogram.getCount(value));
         }
         
     }

2224fb88724d5f38cf7a966ac602ef98678ee336
Usuario <Usuario@10.22.148.82>
2019-10-02 09:19:45 +0100

    Kata1 using Date

--- Práctica 2/src/práctica/pkg2/Histogram.java
+++ Práctica 2/src/práctica/pkg2/Histogram.java
@@ -2,22 +2,23 @@ package práctica.pkg2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Histogram {
-    
+
     private final Map<Integer, Integer> counts= new HashMap<>();
 
     Histogram(int... values) {
         for (int value:values)
-            counts.put(value, counts.get(value)+1);
+            counts.put(value, counts.getOrDefault(value, 0)+1);
     }
     
     public Set<Integer> getValues(){
         return counts.keySet();
     }
-
+    
     public int getCount (int value){
         return counts.get(value);
     }
-    
+
 }
